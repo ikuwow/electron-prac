17783ca0363b617ecfcdd80642665ac9590b6ab6
Ikuo Degawa <degawa@ikuwow.com>
2016-07-20 22:16:42 +0900

    Launch first electron application

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// index.js (main process)
+// - GUI (renderer process)
+// - GUI (renderer process)
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('ready', function(){
+    // create window
+    mainWindow = new BrowserWindow({
+        width: 600, height: 400
+    });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
