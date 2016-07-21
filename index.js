0ab2be82da138a49c4e08715ace8f698d5e70076
Ikuo Degawa <degawa@ikuwow.com>
2016-07-21 23:10:44 +0900

    Add close and activate action

--- index.js
+++ index.js
@@ -10,14 +10,29 @@ const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-app.on('ready', function(){
-    // create window
+function createMainWindow() {
     mainWindow = new BrowserWindow({
-        width: 600, height: 400
+        width: 1080, height: 800
     });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
+}
+
+app.on('ready', function(){
+    createMainWindow();
+});
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
 });
